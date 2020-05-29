d6ff5771135b7f074607837905ac59ea23412b33
Abigail <abbyliguma@gmail.com>
2020-05-29 20:03:18 +0200

    login in page added

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php include('server.php')?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <!--Font link-->
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel&family=PT+Serif&display=swap" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
