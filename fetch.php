3e21eb26ede1eeed01bfad94d6caf4a266189d85
Abigail <abbyliguma@gmail.com>
2020-05-29 20:38:26 +0200

    ordering input by order

--- fetch.php
+++ fetch.php
@@ -18,3 +18,21 @@ if(isset($_POST["search"]["value"]))
     $query .= " WHERE user_id=$user_id ";
 }
 
+/*Order list*/
+if(isset($_POST["order"]))
+{
+ $query .= 'ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
+ ';
+}
+else
+{
+ $query .= ' ORDER BY title ASC ';
+}
+
+$query1 = '';
+
+if($_POST["length"] != -1)
+{
+ $query1 = ' LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+}
+
