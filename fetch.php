ae9b4d141b0012b48e8bce2df954a7c37d4cd456
Abigail <abbyliguma@gmail.com>
2020-05-29 20:36:13 +0200

    database selection and search bar function added

--- fetch.php
+++ fetch.php
@@ -5,3 +5,16 @@ $connect = mysqli_connect("localhost:3308", "root", "", "ags_todo");
 $columns = array('title', 'description');
 $user_id = $_SESSION['user_id'];
 
+/*Select from database*/
+$query = "SELECT * FROM tasks";
+
+/*Search bar code*/
+if(isset($_POST["search"]["value"]))
+{
+ $search_value = $_POST["search"]["value"];
+ 
+ $query .= " WHERE user_id=$user_id AND (title LIKE '%$search_value%' OR description LIKE '%$search_value%') "; 
+} else {
+    $query .= " WHERE user_id=$user_id ";
+}
+
