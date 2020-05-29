ac5f764e5c1b877ef04530a57728bcb0f5df606c
Abigail <abbyliguma@gmail.com>
2020-05-29 20:05:01 +0200

    left side div added on landing

--- login.php
+++ login.php
@@ -10,6 +10,20 @@
   <link href="https://fonts.googleapis.com/css2?family=Cinzel&family=PT+Serif&display=swap" rel="stylesheet">
 </head>
 <body>
-
+<div class="wrap">
+	<div class="top">
+		<div class="inner-top">
+			<p>Why not make your life easier?</p>
+			<p>By creating your very own Todo list<br>you can do just that</p>
+		</div>
+	</div>
+	<div class="bottom">
+		<div class="inner-bottom"></div>
+		<div class="inner-bottom-right">
+			<p>What are you waiting for?<p>
+		<p>Do what others are doing, live your best life<br>go ahead, Login!</p>
+		</div>
+	</div>
+</div>
 </body>
 </html>
