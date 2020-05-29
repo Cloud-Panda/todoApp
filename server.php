033097cf114ec76d2cf30babeeb8fbf9de07b0b5
Abigail <abbyliguma@gmail.com>
2020-05-29 20:14:30 +0200

    form validaiton added

--- server.php
+++ server.php
@@ -10,6 +10,7 @@ $errors = array();
 // connect to the database
 $db = new mysqli('localhost:3308', 'root','', 'ags_todo');
 
+
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
   // receive all input values from the form
@@ -17,5 +18,14 @@ if (isset($_POST['reg_user'])) {
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
 
   
