ad1d6e4a3f7292197f53b08a831c2d0c899a05b9
Abigail <abbyliguma@gmail.com>
2020-05-29 19:59:19 +0200

    bootstrap app table added

--- base.php
+++ base.php
@@ -12,5 +12,30 @@
 <div class="container box container-box">
    <h1 id="title">Todo</h1>
    <br />
+   <!--Table starts-->
+   <div>
+   <br />
+    <div>
+     <button type="button" name="add" id="add" class="btn btn-info add-btn">+ Add item</button>
+    </div>
+    <br />
+    <div id="alert_message"></div>
+    <table id="user_data" class="table  table-back">
+     <thead>
+      <tr>
+       <th>Title</th>
+       <th>Description</th>
+       <th id="options"></th>
+      </tr>
+     </thead>
+    </table>
+   </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.js"></script>
+  <script src="script/script.js"></script>
 </body>
 </html>
