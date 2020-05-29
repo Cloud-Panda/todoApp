81f7be3a6ff5dfe54edfb2543308d40934392755
Abigail <abbyliguma@gmail.com>
2020-05-29 20:23:06 +0200

    register form added

--- register.php
+++ register.php
@@ -11,6 +11,34 @@
 </head>
 <body>
 
+<div class="container">
 
+  	<h2 id="register">Register</h2>
+	
+  <form method="post" action="register.php">
+  	<?php include('inc/errors.php'); ?>
+  	<div class="input-group">
+  	  <input type="text" name="username" placeholder="Username..." value="<?php echo $username; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <input type="email" name="email"placeholder="Email..." value="<?php echo $email; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <input type="password" placeholder="Password..." name="password_1">
+  	</div>
+  	<div class="input-group">
+  	  <input type="password"placeholder="Confirm Pass..." name="password_2">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already a member? <a href="login.php">Sign in</a>
+  	</p>
+  </form>
+ </div>
+
+
+</div>
 </body>
 </html>
