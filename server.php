a266d3b890f2a5aed2a4d10a7203b1d5fb4bf3da
Abigail <abbyliguma@gmail.com>
2020-05-29 20:13:08 +0200

    getting input values from form

--- server.php
+++ server.php
@@ -10,4 +10,12 @@ $errors = array();
 // connect to the database
 $db = new mysqli('localhost:3308', 'root','', 'ags_todo');
 
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
+  
