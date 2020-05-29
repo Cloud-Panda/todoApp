45255e6cc73d4143d193ba13f3867df8669506bb
Abigail <abbyliguma@gmail.com>
2020-05-29 20:44:28 +0200

    script folder added & fetching data from db function added

---
+++ script/script.js
@@ -0,0 +1,18 @@
+ $(document).ready(function(){
+  /*Fetching the data from db*/
+  fetch_data()
+
+  function fetch_data()
+  {
+   var dataTable = $('#user_data').DataTable({
+    "processing" : true,
+    "serverSide" : true,
+    "order" : [],
+    "ajax" : {
+     url:"fetch.php",
+     type:"POST"
+    }
+   });
+  }
+  
+ 
