7e20693eae4698b8eeb7f955880752055861ecdf
Abigail <abbyliguma@gmail.com>
2020-05-29 19:49:01 +0200

    notification message added

--- index.php
+++ index.php
@@ -5,7 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home</title>
 <body>
-
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
 </body>
 </html>
 
