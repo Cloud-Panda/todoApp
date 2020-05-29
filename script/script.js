1d13d7b3cd55cb72b6abd18b75c21e699ea4b73c
Abigail <abbyliguma@gmail.com>
2020-05-29 20:54:51 +0200

    delete function added

--- script/script.js
+++ script/script.js
@@ -78,4 +78,26 @@
     }
    });
    
+   /*Delete function*/
+   $(document).on('click', '.delete', function(){
+    var id = $(this).attr("id");
+    if(confirm("Are you sure you want to remove this?"))
+    {
+     $.ajax({
+      url:"delete.php",
+      method:"POST",
+      data:{id:id},
+      success:function(data){
+       $('#alert_message').html('<div class="alert alert-success">'+data+'</div>');
+       $('#user_data').DataTable().destroy();
+       fetch_data();
+      }
+     });
+     setInterval(function(){
+      $('#alert_message').html('');
+     }, 5000);
+    }
+   });
+  });
+
    
